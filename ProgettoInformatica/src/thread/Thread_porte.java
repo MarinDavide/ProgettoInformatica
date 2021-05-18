b0c1445888807c01e48e54d5a7b02b67a545c49a
glrk03 <marin.davide03@gmail.com>
2021-05-18 17:02:54 +0200

    Progetto finito

---
+++ ProgettoInformatica/src/thread/Thread_porte.java
@@ -0,0 +1,53 @@
+package thread;
+
+import Model.Livelli;
+import view.Gioco;
+
+public class Thread_porte implements Runnable {
+	private Gioco g;
+	private Livelli l;
+
+	public Thread_porte(Gioco g, Livelli l) {
+		this.g = g;
+		this.l = l;
+	}
+
+	@Override
+	public void run() {
+		g.Hidebtn(l.getStanza());
+		int stato = 0;
+		// System.out.println(l.getStanza());
+		System.out.println("Ho iniziato");
+		g.apriChiudi(stato);
+		l.setAperto(true);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		stato++;
+		g.apriChiudi(stato);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		stato++;
+		g.apriChiudi(stato);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		stato++;
+		g.apriChiudi(stato);
+		l.setAperto(false);
+		g.ShowItem(3);
+		System.out.println("ho finito");
+
+	}
+
+}
